040bdd25e910630ba209d64f6f02523569cf31cf
aziz-codes <azizisoffline@gmail.com>
2023-02-05 16:49:44 +0500

    working with loginform

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const btn = document.getElementById("btnSend");
-btn.addEventListener("click", () => {
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", "backend/index.php", true);
-  xhr.onload = () => {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      if (xhr.status === 200) {
-        let data = xhr.response;
-        console.log(data);
-      }
-    }
-  };
-  xhr.send();
-  //   console.log("hello");
+// const btn = document.getElementById("btnSend");
+const form = document.getElementById("form-login");
+console.log("js");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("first");
 });
+
+// btn.addEventListener("click", () => {
+//   let xhr = new XMLHttpRequest();
+//   xhr.open("POST", "backend/index.php", true);
+//   xhr.onload = () => {
+//     if (xhr.readyState === XMLHttpRequest.DONE) {
+//       if (xhr.status === 200) {
+//         let data = xhr.response;
+//         console.log(data);
+//       }
+//     }
+//   };
+//   xhr.send();
+//   //   console.log("hello");
+// });
