9593f8dd18feae6d138e34824b527a12cf4929dd
aziz-codes <azizisoffline@gmail.com>
2023-02-05 17:24:00 +0500

    server response using ajax for login

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 // const btn = document.getElementById("btnSend");
+// ..............globals.......................
+
+// ............globals..........................
 const form = document.getElementById("form-login");
-console.log("js");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("first");
-});
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "backend/login.php", true);
+  xhr.onload = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      if (xhr.status === 200) {
+        let data = xhr.response;
+        console.log(data);
+      } else {
+        console.log("somehtign ");
+      }
+    }
+  };
+  // using form data..
 
-// btn.addEventListener("click", () => {
-//   let xhr = new XMLHttpRequest();
-//   xhr.open("POST", "backend/index.php", true);
-//   xhr.onload = () => {
-//     if (xhr.readyState === XMLHttpRequest.DONE) {
-//       if (xhr.status === 200) {
-//         let data = xhr.response;
-//         console.log(data);
-//       }
-//     }
-//   };
-//   xhr.send();
-//   //   console.log("hello");
-// });
+  let formData = new FormData(form);
+
+  xhr.send(formData);
+});
